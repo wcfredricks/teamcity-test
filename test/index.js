29f78f95378af9e819a47efb1f63aaab83254289
wcfredricks <wcfredricks@gmail.com>
2016-01-20 07:49:12 -0500

    fixed formatting

--- test/index.js
+++ test/index.js
@@ -4,12 +4,10 @@ var assert = require('assert');
 // var teamcityTest = require('../lib');
 
 describe('teamcity-test', function () {
-
   it('should have unit test!', function () {
     assert(true, 'we expected this package author to add actual unit tests.');
   });
-
   it('should have unit test!', function () {
-    assert(true, 1 + 3 == 4);
+    assert(true, 1 + 3 === 4);
   });
 });
