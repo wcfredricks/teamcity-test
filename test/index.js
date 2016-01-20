cb00efa6bfecca4458b2e6f8c74cab0b2957d125
wcfredricks <wcfredricks@gmail.com>
2016-01-20 07:43:02 -0500

    another checkin

--- test/index.js
+++ test/index.js
@@ -4,7 +4,12 @@ var assert = require('assert');
 // var teamcityTest = require('../lib');
 
 describe('teamcity-test', function () {
+
   it('should have unit test!', function () {
     assert(true, 'we expected this package author to add actual unit tests.');
+  });
+
+  it('should have unit test!', function () {
+    assert(true, 1 + 3 == 4);
   });
 });
